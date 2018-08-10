478af535c33e4052bf832eb1d72e964e4efbd764
Vincenzo Casbarro <vincenzo.casbarro@gmail.com>
2018-08-10 18:32:35 -0400

    add arrays and if-else logic to card game

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -1,8 +1,20 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-alert("Up and running!");
+// alert("Up and running!");
+// console.log("Up and running!");
+
+var cards = ["queen", "king", "queen", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+console.log("User flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again");
+  }
+};
