aae63a5de281249cedc6a4a065b4dad0a3851ef5
Vincenzo Casbarro <vincenzo.casbarro@gmail.com>
2018-08-12 21:12:07 -0400

    modularize flipCard and checkForMatch with variables

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -1,20 +1,26 @@
 // alert("Up and running!");
-// console.log("Up and running!");
+console.log("Up and running!");
 
+// Arrays
 var cards = ["queen", "king", "queen", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-
-console.log("User flipped " + cardsInPlay);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again");
-  }
+// Functions
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again");
+    }
+  };
 };
+var flipCard = function (cardID) {
+  console.log("User flipped " + cards[cardID]);
+  cardsInPlay.push(cards[cardID]);
+  checkForMatch();
+};
+
+// Scripts
+flipCard(0);
+flipCard(1);
